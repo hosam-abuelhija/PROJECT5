95928fb5f650f2adc8b8bab2269e34005e8a129d
hosam-abuelhija <hosam.adnan123@gmail.com>
2024-08-03 19:10:47 +0300

    adminviews

--- Project5/Project5/Controllers/AdminController.cs
+++ Project5/Project5/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,47 @@ namespace Project5.Controllers
     {
         // GET: Admin
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult LocalListsApplications()
+        {
+            return View();
+        }
+        public ActionResult PartyListsApplications()
+        {
+            return View();
+        }
+        public ActionResult ADsApplications()
+        {
+            return View();
+        }
+        public ActionResult DebatesApplications()
+        {
+            return View();
+        }
+        public ActionResult Results()
+        {
+            return View();
+        }
+        public ActionResult Feedbacks()
+        {
+            return View();
+        }
+        public ActionResult ViewCandedates()
+        {
+            return View();
+        }
+        public ActionResult AddAdmins()
         {
             return View();
         }
